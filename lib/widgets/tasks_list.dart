b3bb7043af2faf9284649ca7eb8e023dd96782d3
keigo <b1951154@planet.kanazawa-it.ac.jp>
2022-04-09 10:33:37 +0900

    ListViewBuilderの導入

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_state_management/widgets/task_title.dart';
+import 'package:flutter_state_management/models/task.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+
+  List<Task> tasks = [
+    Task(name:  'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name:  'Buy bread'),
+  ];
+
+
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        TaskTitle(),
-        TaskTitle(),
-        TaskTitle(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context , index){
+        return TaskTitle(
+            taskTitle: tasks[index].name,
+            isChecked: tasks[index].isDone,
+            checkboxCallback: (bool checkboxState){
+              setState(() {
+                tasks[index].toggleDone();
+              });
+            }
+
+        );
+      },
+      itemCount: tasks.length,
     );
   }
 }
